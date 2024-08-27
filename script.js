a2b322a6d0ce7ed7eea30bfb934b4f6dd3569cfa
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2024-08-27 09:25:41 +0000

    addition files

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const menuBtn=document.querySelector('.menu');
 const navItem=document.querySelector('.nav__items');
 const navLinks=document.querySelectorAll('.nav__items .nav__links');
- console.log(navLinks)
+
+
 menuBtn.addEventListener('click',()=>{
   navItem.classList.toggle('show')
   menuBtn.classList.toggle('toggle')
@@ -16,3 +17,31 @@ navLinks.forEach(navLink=>{
     }
   })
 })
+
+     
+      const experience=document.querySelector('.experience')
+      const contact=document.querySelector('.contact__section');
+      experience.addEventListener("click",()=>[
+      contact.classList.toggle('move')
+      ])
+   
+    const aboutLists=document.querySelectorAll('.about__list');
+    aboutLists.forEach(aboutList=>{
+      const dropdown=aboutList.querySelector('.dropdown');
+      const head =aboutList.querySelector('.about__list p');
+      console.log(head)
+      aboutList.addEventListener('click',()=>{
+       
+      aboutLists.forEach(list=>{
+       
+       const  listDropdown=list.querySelector(' .dropdown');
+       if( listDropdown!==dropdown){
+        listDropdown.classList.remove('active');
+        // contact.style.paddingBlock="50rem 0"
+       } 
+       });
+      head.classList.toggle('change')
+      dropdown.classList.toggle('active')
+   
+     })
+ });
